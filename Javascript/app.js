7347f420237091e5251a6ede327144c825ff0471
Paul Barroso <paulbg1993@gmail.com>
2020-10-21 08:58:49 +0200

    Fixed comparison methods

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,4 +1,3 @@
-
      // Create Animal Class with Dino/Human subclass 
     function Animal (species, weight, height, diet, image){
         this.species=species;
@@ -25,7 +24,7 @@
     const dino1 = new Dino(
         "Triceratops",
         5895,
-        114,
+        3474,
         "herbavor",
         "North America",
         "Late Cretaceous",
@@ -34,7 +33,7 @@
     const dino2 = new Dino(
         "Tyrannosaurus Rex",
         5400,
-        144,
+        4390,
         "carnivor",
         "North America",
         "Late Cretaceous",
@@ -43,7 +42,7 @@
     const dino3 = new Dino(
         "Anklyosaurus",
         4760,
-        55,
+        1676,
         "herbavor",
         "North America",
         "Late Cretaceous",
@@ -52,7 +51,7 @@
     const dino4 = new Dino(
         "Brachiosaurus",
         31700,
-        372,
+        11338,
         "herbavor",
         "North America",
         "Late Jurasic",
@@ -61,7 +60,7 @@
     const dino5 = new Dino(
         "Stegosaurus",
         5260,
-        79,
+        2408,
         "herbavor",
         "North America, Europe, Asia",
         "Late Jurasic to Early Cretaceous",
@@ -70,7 +69,7 @@
     const dino6 = new Dino(
         "Elasmosaurus",
         7260,
-        59,
+        1799,
         "carnivor",
         "North America",
         "Late Cretaceous",
@@ -79,7 +78,7 @@
     const dino7 = new Dino(
         "Pteranodon",
         20,
-        20,
+        610,
         "carnivor",
         "North America",
         "Late Cretaceous",
@@ -88,15 +87,12 @@
     const dino8 = new Dino(
         "Pigeon",
         0.2,
-        9,
+        27,
         "herbavor",
         "World Wide",
         "Holocene",
         "All birds are living dinosaurs."
     );
-    
-console.log(dino5);
-
     // Create Human Object
 
 const human = new Human;
@@ -114,10 +110,6 @@ const userData = () => {
     return human
 }
 
-
-console.log(typeof human)
-console.log(userData())
-
 //Generate dino array and shuffle function. Stackoverflow shuffle method (https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array)
 
 const myArray = [dino1, dino2, dino3, dino4, dino5, dino6, dino7, dino8];
@@ -131,8 +123,6 @@ function shuffle(myArray) {
 };
 
 myArray.splice(4, 0, human); //Put value human in the middle of the array (https://stackoverflow.com/questions/586182/how-to-insert-an-item-into-an-array-at-a-specific-index-javascript)
-
-console.log(myArray)
 
 //Dino method 1 (weight)
 
@@ -154,7 +144,7 @@ Dino.prototype.methodHeight = function(human) {
     } else if (this.height == human.height){
         return `${this.species} and you have the same height!`
     } else {
-        return `You are ${human.height - this.height}kg. taller than a ${this.species}.`
+        return `You are ${human.height - this.height}cm. taller than a ${this.species}.`
     }
   };
 
@@ -166,12 +156,16 @@ Dino.prototype.methodHeight = function(human) {
     : 
     `Welp. You two are quite different when it comes to diet.`;
 
-//Random comparison facts 
-
-function factsRandom(){
-
+//Random number that is not repeated from a dino array. Human and pigeon are not included. https://stackoverflow.com/questions/18806210/generating-non-repeating-random-numbers-in-js/18806417
+function* randomNumber(myArray) {
+    let i = myArray.length;
+    while (i--) {
+        yield myArray.splice(Math.floor(Math.random() * (i+1)), 1)[0];
+    }
 }
-    
+
+let ranNums = randomNumber([dino1, dino2, dino3, dino4, dino5, dino6, dino7]);
+   
 // Generate Tiles
 
 const getHumanTile = () => {
@@ -186,26 +180,63 @@ const getDinoTile = (dino) => {
     return `<div class="grid-item">
     <h3>${dino.species}</h3>
     <img src="images/${dino.species.toLowerCase()}.png">
-    <h4>${dino.fact}</h4> 
+    <h4>${dino.fact}</h4>
     </div>`
 }
 
-//Get a HTML string back with the dino and human tiles and placed it on .grid
+const getDinoTileDiet = (dino) => {
+    return `<div class="grid-item">
+    <h3>${dino.species}</h3>
+    <img src="images/${dino.species.toLowerCase()}.png">
+    <h4>${dino.methodDiet(human)}</h4>
+    </div>`
+}
 
+const getDinoTileWeight = (dino) => {
+    return `<div class="grid-item">
+    <h3>${dino.species}</h3>
+    <img src="images/${dino.species.toLowerCase()}.png">
+    <h4>${dino.methodWeight(human)}</h4>
+    </div>`
+}
+
+const getDinoTileHeight = (dino) => {
+    return `<div class="grid-item">
+    <h3>${dino.species}</h3>
+    <img src="images/${dino.species.toLowerCase()}.png">
+    <h4>${dino.methodHeight(human)}</h4>
+    </div>`
+}
+
+//Generate a new compare button
+const getButton = () => {
+    return `<div id='newBtn'>Compare Me Again!</div>`
+}
+
+//Get a HTML string back with the dino and human tiles and placed it on .grid
 function fillGrid(){
+    let random1 =ranNums.next().value; //Generate a random dino from the array that is not pigeon or human
+    let random2 =ranNums.next().value;
+    let random3 =ranNums.next().value;
     let str = '';
     for (let i = 0; i < myArray.length; i++) {
         if (i == 4) {
             str = str + getHumanTile();
+        } else if (random1 == myArray[i]) {
+            str = str + getDinoTileDiet(random1);
+        } else if (random2 == myArray[i]) {
+            str = str + getDinoTileWeight(random2);
+        } else if (random3 == myArray[i]) {
+            str = str + getDinoTileHeight(random3); 
         } else {
             str = str + getDinoTile(myArray[i]);
         }   
     };
+    str = str + getButton();
     return str;
 }
 
 // Remove form from screen
-
 const form =  document.getElementById('dino-compare');
 
 function removeForm() {
@@ -214,8 +245,7 @@ function removeForm() {
 
 // On button click, prepare and display infographic
     const button = document.getElementById('btn');
-    
-    button.addEventListener('click', onClick)
+    button.addEventListener('click', onClick);
 
     function onClick(){
         userData();
